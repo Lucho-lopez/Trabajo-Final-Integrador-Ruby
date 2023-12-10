e49364bc3c7366cff61e3c842594a1d567cae230
Lopez Luciano <lopezluciano_@hotmail.com>
2023-12-10 20:20:58 -0300

    It is validated that the links to be shortened are not from the chq.to domain

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,11 +3,13 @@ class Link < ApplicationRecord
     
     belongs_to :user
     has_many :visit_infos, dependent: :destroy
-    validates :url, presence: true
+    validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "es invalido, un ejemplo de una url valida es http://127.0.0.1:3000"} 
     validates :link_password, presence: true, if: -> { link_type == 'private' }
     validates :expires_at, presence: true, if: -> { link_type == 'temporal' }
+    validate :url_not_same_as_current_domain 
     before_create :generate_unique_token
 
+    
     HUMANIZED_ATTRIBUTES = {
       link_name: "Nombre del link",
       link_type: "Tipo de link",
@@ -52,6 +54,16 @@ class Link < ApplicationRecord
 
     def create_visit_info(ip_address, visited_at)
       visit_infos.create(ip_address: ip_address, visited_at: visited_at)
+    end
+
+    def url_not_same_as_current_domain
+      return unless url.present?
+  
+      current_domain = 'chq.to'
+  
+      if URI.parse(url).host == current_domain
+        errors.add(:url, "no puede ser igual al dominio actual")
+      end
     end
     
   end
